6f0667fb03356275afb09726975ec4d537ee416c
a1da4 <ai.the.taichi@gmail.com>
2021-11-23 14:35:31 +0900

    add description

--- preprocess/pseudowords/README.md
+++ preprocess/pseudowords/README.md
@@ -11,3 +11,38 @@
 ## others
 - ioutils.py
 - matrixutils.py
+
+## Example
+### 0. train cooccur/word vectors using target documents
+- Cooccur matrix
+- PPMI matrix
+- PMI-SVDjoint, c
+- Word2Vec with alignment
+- Dynamic Word Embeddings
+
+### 1. obtain pair of words
+```
+$python3 obtain_wordpair.py --word_matrices {matrix_1} {matrix_2} \
+  --id2word {id2word.pkl} \
+  > wordpairs.txt
+```
+
+### 2. replace corpus with 50 pairs of words
+#### 2.a replace all
+```
+$python3 replace_words.py --file_path {target_corpus} \
+  --wordpair_list {wordpairs.txt}
+
+$python3 convert_freq_replaced.py --word2freq {word2freq_target} \
+  --wordpair_list {wordpairs.txt}
+```
+
+#### 2.b with rate (0.1, 0.2, 0.3, 0.4, 0.5)
+
+```
+$python3 replace_with_rate.py --file_path {target_corpus} \
+  --word_to_freq {word2freq_target.pkl} \
+  --wordpair_list {wordpairs.txt} 
+```
+
+
--- preprocess/pseudowords/obtain_wordpair.py
+++ preprocess/pseudowords/obtain_wordpair.py
@@ -7,22 +7,18 @@ from matrixutils import cos_sim, make_similarity_matrix
 
 
 def main(args):
-    """
-    1. V*V ペアで共起行列の内積を計算
-    2. 内積が0となるペアを抽出
-    """
     print(args)
     id2word = load_pickle(args.id2word)
-    if args.similarity_matrixes is not None:
+    if args.similarity_matrices is not None:
         print("1. load sims_mat ...", end="")
-        sims_path_base, sims_path_other = args.similarity_matrixes
+        sims_path_base, sims_path_other = args.similarity_matrices
         sims_mat_base = np.load(sims_path_base)
         sims_mat_other = np.load(sims_path_other)
         print("done")
     else:
         print("1. create sims_mat ...", end="")
-        assert args.cooccur_matrixes is not None, "COOCCUR NOT FOUND ERROR"
-        c_path_base, c_path_other = args.cooccur_matrixes
+        assert args.word_matrices is not None, "MATRICES NOT FOUND ERROR"
+        c_path_base, c_path_other = args.word_matrices
         c_mat_base = np.load(c_path_base)
         c_mat_other = np.load(c_path_other)
         sims_mat_base = make_similarity_matrix(c_mat_base, id2word)
@@ -55,10 +51,10 @@ def main(args):
 def cli_main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "-c", "--cooccur_matrixes", nargs=2, help="path of co occur matrixes"
+        "-m", "--word_matrices", nargs=2, help="path of cooccur/wordvector matrices"
     )
     parser.add_argument(
-        "-s", "--similarity_matrixes", nargs=2, help="path of similarity matrixes"
+        "-s", "--similarity_matrices", nargs=2, help="path of similarity matrices"
     )
     parser.add_argument("-p", "--id2word", help="path of index2word")
     args = parser.parse_args()
